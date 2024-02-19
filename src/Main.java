785a8b97155cad9323fa145d1585b05ff1f66b68
VolticuS6 <100294967+Stuchellbj21@users.noreply.github.com>
2024-02-19 14:09:02 -0500

    The one we did in class 2/19

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        class Time{
+            String string;
+
+        }
+        class Course{
+
+        }
+        class Search{
+
+        }
+        class Schedule{
+
+        }
+        class User{
+
+        }
+
+
+
+        String username;
+        String password;
         System.out.println("Hello world!");
         System.out.println("Bye-bye world!");
         //dsfa
